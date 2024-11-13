93b1d8ca4da324577c80293a536555857847a254
Eduardo Tartas <eduardos.tartas@gmail.com>
2024-11-13 18:57:47 -0400

    funcionando botão buy5

--- dist/index.js
+++ dist/index.js
@@ -1,61 +1,62 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', () => {
-    class Entity {
-        constructor(price, cookiesValue) {
-            this.price = price;
-            this.quantity = 0;
-            this.cookiesValue = cookiesValue;
-            this.cookiesPerSecond = 0;
-        }
-        buy1() {
-            if (cookie.quantity >= this.price) {
-                cookie.quantity -= this.price;
-                this.price = Math.round(this.price * 1.15);
-                this.quantity++;
-                this.cookiesPerSecond += this.cookiesValue;
-            }
-        }
-        buy5() {
-            if (cookie.quantity >= (this.price * 5)) {
-                cookie.quantity -= (this.price * 5);
-                for (let i = 0; i < 5; i++) {
-                    this.price = Math.round(this.price * 1.15);
-                }
-                this.quantity += 5;
-                this.cookiesPerSecond += (this.cookiesValue * 5);
-            }
-        }
-        getPrice() { return this.price; }
-        setPrice(price) { this.price = price; }
-        getQuantity() { return this.quantity; }
-        setQuantity(quantity) { this.quantity = quantity; }
-        getCookiesValue() { return this.cookiesValue; }
-        setCookiesValue(cookiesValue) { this.cookiesValue = cookiesValue; }
-        getCookiesPerSecond() { return this.cookiesPerSecond; }
-        setCookiesPerSecond(cookiesPerSecond) { this.cookiesPerSecond = cookiesPerSecond; }
+class Entity {
+    constructor(price, cookiesValue) {
+        this.price = price;
+        this.quantity = 0;
+        this.cookiesValue = cookiesValue;
+        this.cookiesPerSecond = 0;
     }
-    const cookie = new Entity(0, 0);
-    cookie.setQuantity(10000000);
-    const grandma = new Entity(50, 2);
-    console.log(grandma);
-    //contador e cookie principal
+    buy1() {
+        if (cookie.quantity >= this.price) {
+            cookie.quantity -= this.price;
+            this.price = Math.round(this.price * 1.15);
+            this.quantity++;
+            this.cookiesPerSecond += this.cookiesValue;
+        }
+    }
+    buy5() {
+        if (cookie.quantity >= (this.price * 5)) {
+            cookie.quantity -= (this.price * 5);
+            for (let i = 0; i < 5; i++) {
+                this.price = Math.round(this.price * 1.15);
+            }
+            this.quantity += 5;
+            this.cookiesPerSecond += (this.cookiesValue * 5);
+        }
+    }
+    getPrice() { return this.price; }
+    getQuantity() { return this.quantity; }
+    getCookiesValue() { return this.cookiesValue; }
+    getCookiesPerSecond() { return this.cookiesPerSecond; }
+    setPrice(price) { this.price = price; }
+    setQuantity(quantity) { this.quantity = quantity; }
+    setCookiesValue(cookiesValue) { this.cookiesValue = cookiesValue; }
+    setCookiesPerSecond(cookiesPerSecond) { this.cookiesPerSecond = cookiesPerSecond; }
+}
+const cookie = new Entity(0, 0);
+cookie.setQuantity(10000000);
+const grandma = new Entity(50, 1);
+document.addEventListener('DOMContentLoaded', () => {
+    //cookie's querySelector
     const counter = document.querySelector('#counter');
     const cookieImage = document.querySelector('#cookieImage');
-    //grandma preço quantidade=>
-    const grandmaCounter = document.querySelector('#grandma-quantity');
+    //gradma's querySelector
+    const grdCounter = document.querySelector('#grandma-quantity');
+    const grdPrice = document.querySelector('#grandma-price');
     const grdBuy1 = document.querySelector('#buy-1-grandma');
     const grdBuy5 = document.querySelector('#buy-5-grandma');
-    const grdPrice = document.querySelector('#grandma-price');
+    //status' update
     function update() {
         counter.innerHTML = `${cookie.getQuantity()}`;
-        grandmaCounter.innerHTML = `${grandma.getQuantity()}`;
+        grdCounter.innerHTML = `${grandma.getQuantity()}`;
         grdPrice.innerHTML = `${grandma.getPrice()}`;
     }
-    //função de gerar cookie clicando
+    //cookie's click
     cookieImage.addEventListener('click', () => {
         cookie.buy1();
         update();
     });
+    //grandma's buy 1 and buy 5
     grdBuy1.addEventListener('click', () => {
         grandma.buy1();
         update();
@@ -64,4 +65,10 @@ document.addEventListener('DOMContentLoaded', () => {
         grandma.buy5();
         update();
     });
+    function second() {
+        let totalCookiePerSecond = 0;
+        totalCookiePerSecond += grandma.getCookiesPerSecond();
+        cookie.setQuantity(cookie.getQuantity() + totalCookiePerSecond);
+    }
+    setInterval(second, 500);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,103 @@
-document.addEventListener('DOMContentLoaded', () => {
+class Entity {
+    private price:number;
+    private quantity:number;
+    private cookiesValue:number;
+    private cookiesPerSecond:number;
 
-    class Entity {
-        private price:number;
-        private quantity:number;
-        private cookiesValue:number;
-        private cookiesPerSecond:number;
+    constructor(price:number, cookiesValue:number){
+        this.price = price;
+        this.quantity = 0;
+        this.cookiesValue = cookiesValue;
+        this.cookiesPerSecond = 0;
+    }
 
-        constructor(price:number, cookiesValue:number){
-            this.price = price;
-            this.quantity = 0;
-            this.cookiesValue = cookiesValue;
-            this.cookiesPerSecond = 0;
+    buy1() : void{
+        if(cookie.quantity>=this.price){
+            cookie.quantity -=this.price
+            this.price = Math.round(this.price* 1.15);
+            this.quantity ++;
+            this.cookiesPerSecond += this.cookiesValue;
         }
-
-        buy1() : void{
-            if(cookie.quantity>=this.price){
-                cookie.quantity -=this.price
-                this.price = Math.round(this.price* 1.15);
-                this.quantity ++;
-                this.cookiesPerSecond += this.cookiesValue;
-            }
-        }
-
-
-        buy5() : void{
-            if (cookie.quantity >= (this.price * 5)) {
-                cookie.quantity -= (this.price * 5);
-                for (let i = 0; i < 5; i++) {
-                    this.price = Math.round(this.price * 1.15);
-                }
-                this.quantity += 5;
-                this.cookiesPerSecond += (this.cookiesValue * 5);
-            }
-        }
-    
-
-        public getPrice(): number {return this.price;}
-
-        public setPrice(price: number): void {this.price = price;}
-
-        public getQuantity(): number {return this.quantity;}
-
-        public setQuantity(quantity: number): void {this.quantity = quantity;}
-
-        public getCookiesValue(): number {return this.cookiesValue;}
-
-        public setCookiesValue(cookiesValue: number): void {this.cookiesValue = cookiesValue;}
-
-        public getCookiesPerSecond(): number {return this.cookiesPerSecond;}
-
-        public setCookiesPerSecond(cookiesPerSecond: number): void {this.cookiesPerSecond = cookiesPerSecond;}
-
     }
 
 
+    buy5() : void{
+        if (cookie.quantity >= (this.price * 5)) {
+            cookie.quantity -= (this.price * 5);
+            for (let i = 0; i < 5; i++) {
+                this.price = Math.round(this.price * 1.15);
+            }
+            this.quantity += 5;
+            this.cookiesPerSecond += (this.cookiesValue * 5);
+        }
+    }
+
+    public getPrice(): number {return this.price;}
+
+    public getQuantity(): number {return this.quantity;}
+
+    public getCookiesValue(): number {return this.cookiesValue;}
+
+    public getCookiesPerSecond(): number {return this.cookiesPerSecond;}
 
 
-    const cookie = new Entity(0,0)
+    public setPrice(price: number): void {this.price = price;}
+
+    public setQuantity(quantity: number): void {this.quantity = quantity;}
+
+    public setCookiesValue(cookiesValue: number): void {this.cookiesValue = cookiesValue;}
+
+    public setCookiesPerSecond(cookiesPerSecond: number): void {this.cookiesPerSecond = cookiesPerSecond;}
+
+}
+
+const cookie = new Entity(0,0)
     cookie.setQuantity(10000000);
 
-    const grandma = new Entity(50, 2);
-    console.log(grandma);
+const grandma = new Entity(50, 1);
 
-        //contador e cookie principal
-        const counter = document.querySelector('#counter')!;
+document.addEventListener('DOMContentLoaded', () => {
+
+        //cookie's querySelector
+        const counter     = document.querySelector('#counter')!;
         const cookieImage = document.querySelector('#cookieImage')!;
 
-        //grandma preço quantidade=>
-        const grandmaCounter = document.querySelector('#grandma-quantity')!;
-        const grdBuy1 = document.querySelector('#buy-1-grandma')!;
-        const grdBuy5 = document.querySelector('#buy-5-grandma')!;
-        const grdPrice = document.querySelector('#grandma-price')!;
-
-        function update(){
-            counter.innerHTML = `${cookie.getQuantity()}`;
-            grandmaCounter.innerHTML = `${grandma.getQuantity()}`;
-            grdPrice.innerHTML = `${grandma.getPrice()}`;
+        //gradma's querySelector
+        const grdCounter = document.querySelector('#grandma-quantity')!;
+        const grdPrice   = document.querySelector('#grandma-price')!;
+        const grdBuy1    = document.querySelector('#buy-1-grandma')!;
+        const grdBuy5    = document.querySelector('#buy-5-grandma')!;
+        
+        //status' update
+        function update(): void{
+            counter.innerHTML    = `${cookie.getQuantity()}`;
+            grdCounter.innerHTML = `${grandma.getQuantity()}`;
+            grdPrice.innerHTML   = `${grandma.getPrice()}`;
         }
         
-        //função de gerar cookie clicando
-
+        //cookie's click
         cookieImage.addEventListener('click', () =>{
             cookie.buy1();
             update();
         });
         
+        //grandma's buy 1 and buy 5
         grdBuy1.addEventListener('click', ()=>{
             grandma.buy1();
-            update()});
+            update()
+        });
 
         grdBuy5.addEventListener('click', ()=>{
             grandma.buy5();
-            update()});
+            update();
+        });         
     
+    function second(): void{
+        let totalCookiePerSecond :number = 0;
+        totalCookiePerSecond += grandma.getCookiesPerSecond();
+        cookie.setQuantity(cookie.getQuantity() + totalCookiePerSecond)
+    }   
 
-            
+    setInterval(second,500);
+
 });
