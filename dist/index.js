892f6976234af31a8e2631ec23c5daae8aaff3e3
Eduardo Tartas <eduardos.tartas@gmail.com>
2024-11-14 17:49:08 -0400

    adicinando features

--- dist/index.js
+++ dist/index.js
@@ -12,13 +12,10 @@ class Entity {
         this.price5 = this.price;
         for (let i = 0; i < 5; i++) {
             this.price5 = Math.round(this.price5 * this.priceScale);
-            console.log(i);
-            console.log(this.price5);
         }
         return this.price5;
     }
     buy1() {
-        console.log(this.price * 5);
         if (cookie.quantity >= this.price) {
             cookie.quantity -= this.price;
             this.price = Math.round(this.price * this.priceScale);
@@ -31,7 +28,7 @@ class Entity {
             cookie.quantity -= this.price5;
             this.price = this.price5;
             this.quantity += 5;
-            this.cookiesPerSecond += (this.cookiesValue * 5);
+            cookie.cookiesPerSecond += (this.cookiesValue * 5);
         }
     }
     getPrice() { return this.price; }
@@ -44,7 +41,7 @@ class Entity {
     setCookiesPerSecond(cookiesPerSecond) { this.cookiesPerSecond = cookiesPerSecond; }
 }
 const cookie = new Entity(0, 0, 0);
-cookie.setQuantity(50000);
+cookie.setQuantity(49);
 const grandma = new Entity(50, 1, 1.15);
 const farm = new Entity(200, 2, 1.20);
 const mine = new Entity(500, 5, 1.40);
@@ -56,27 +53,34 @@ document.addEventListener('DOMContentLoaded', () => {
     //gradma's querySelector
     const grdQuantity = document.querySelector('#grandma-quantity');
     const grdPrice = document.querySelector('#grandma-price');
+    const grdPrice5 = document.querySelector('#grandma-price5');
     const grdBuy1 = document.querySelector('#buy-1-grandma');
     const grdBuy5 = document.querySelector('#buy-5-grandma');
     //farm's querySelector
     const farmQuantity = document.querySelector('#farm-quantity');
     const farmPrice = document.querySelector('#farm-price');
+    const farmPrice5 = document.querySelector('#farm-price5');
     const farmBuy1 = document.querySelector('#buy-1-farm');
     const farmBuy5 = document.querySelector('#buy-5-farm');
     //mine's querySelector
     const mineQuantity = document.querySelector('#mine-quantity');
     const minePrice = document.querySelector('#mine-price');
+    const minePrice5 = document.querySelector('#mine-price5');
     const mineBuy1 = document.querySelector('#buy-1-mine');
     const mineBuy5 = document.querySelector('#buy-5-mine');
+    const hover = document.querySelector('.button:hover');
     //status' update
     function update() {
         counter.innerHTML = `${cookie.getQuantity()}`;
         grdQuantity.innerHTML = `${grandma.getQuantity()}`;
         grdPrice.innerHTML = `${grandma.getPrice()}`;
+        grdPrice5.innerHTML = `${grandma.cost5()}`;
         farmQuantity.innerHTML = `${farm.getQuantity()}`;
         farmPrice.innerHTML = `${farm.getPrice()}`;
+        farmPrice5.innerHTML = `${farm.cost5()}`;
         mineQuantity.innerHTML = `${mine.getQuantity()}`;
         minePrice.innerHTML = `${mine.getPrice()}`;
+        minePrice5.innerHTML = `${mine.cost5()}`;
     }
     //cookie's click
     cookieImage.addEventListener('click', () => {
@@ -87,6 +91,16 @@ document.addEventListener('DOMContentLoaded', () => {
     grdBuy1.addEventListener('click', () => {
         grandma.buy1();
         update();
+    });
+    grdBuy1.addEventListener('mouseover', () => {
+        if (cookie.getQuantity() >= grandma.getPrice()) {
+            grdBuy1.classList.remove('button-false');
+            grdBuy1.classList.add('button-true');
+        }
+        else {
+            grdBuy1.classList.remove('button-true');
+            grdBuy1.classList.add('button-false');
+        }
     });
     grdBuy5.addEventListener('click', () => {
         grandma.buy5();
--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,11 @@ class Entity {
         this.price5 = this.price;
         for (let i = 0; i < 5; i++) {
             this.price5 = Math.round(this.price5 * this.priceScale);
-            console.log(i)
-            console.log(this.price5)
         }
         return this.price5;
     }
 
     buy1() : void{
-        console.log(this.price *5)
         if(cookie.quantity>=this.price){
             cookie.quantity -=this.price
             this.price = Math.round(this.price * this.priceScale);
@@ -42,7 +39,7 @@ class Entity {
             cookie.quantity -= this.price5;
             this.price = this.price5;
             this.quantity += 5;
-            this.cookiesPerSecond += (this.cookiesValue * 5);
+            cookie.cookiesPerSecond += (this.cookiesValue * 5);
         }
     }
 
@@ -66,7 +63,7 @@ class Entity {
 }
 
 const cookie = new Entity(0,0,0)
-    cookie.setQuantity(50000);
+    cookie.setQuantity(49);
 
 const grandma = new Entity(50, 1, 1.15);
 
@@ -84,31 +81,40 @@ document.addEventListener('DOMContentLoaded', () => {
     //gradma's querySelector
     const grdQuantity = document.querySelector('#grandma-quantity')!;
     const grdPrice    = document.querySelector('#grandma-price')!;
+    const grdPrice5    = document.querySelector('#grandma-price5')!;
     const grdBuy1     = document.querySelector('#buy-1-grandma')!;
     const grdBuy5     = document.querySelector('#buy-5-grandma')!;
     //farm's querySelector
     const farmQuantity = document.querySelector('#farm-quantity')!;
     const farmPrice    = document.querySelector('#farm-price')!;
+    const farmPrice5    = document.querySelector('#farm-price5')!;
     const farmBuy1     = document.querySelector('#buy-1-farm')!;
     const farmBuy5     = document.querySelector('#buy-5-farm')!;
     //mine's querySelector
     const mineQuantity = document.querySelector('#mine-quantity')!;
     const minePrice    = document.querySelector('#mine-price')!;
+    const minePrice5    = document.querySelector('#mine-price5')!;
     const mineBuy1     = document.querySelector('#buy-1-mine')!;
     const mineBuy5     = document.querySelector('#buy-5-mine')!;
-        
+
+    const hover = document.querySelector('.button:hover')!;
+
+
     //status' update
     function update(): void{
         counter.innerHTML    = `${cookie.getQuantity()}`  
 
         grdQuantity.innerHTML = `${grandma.getQuantity()}`;
-        grdPrice.innerHTML   = `${grandma.getPrice()}`  
+        grdPrice.innerHTML   = `${grandma.getPrice()}`
+        grdPrice5.innerHTML = `${grandma.cost5()}`  
 
         farmQuantity.innerHTML = `${farm.getQuantity()}`;
-        farmPrice.innerHTML   = `${farm.getPrice()}`   
+        farmPrice.innerHTML   = `${farm.getPrice()}`
+        farmPrice5.innerHTML = `${farm.cost5()}`   
 
         mineQuantity.innerHTML = `${mine.getQuantity()}`;
         minePrice.innerHTML   = `${mine.getPrice()}`;
+        minePrice5.innerHTML = `${mine.cost5()}`
         }
         
         //cookie's click
@@ -117,6 +123,8 @@ document.addEventListener('DOMContentLoaded', () => {
             update();
             
         });
+
+        
         
         //grandma's buy 1 and buy 5
         grdBuy1.addEventListener('click', ()=>{
@@ -124,10 +132,24 @@ document.addEventListener('DOMContentLoaded', () => {
             update()
         });
 
+         grdBuy1.addEventListener('mouseover', () => {
+            if(cookie.getQuantity()>= grandma.getPrice()){
+                grdBuy1.classList.remove('button-false')
+                grdBuy1.classList.add('button-true')
+            }
+            else{
+                grdBuy1.classList.remove('button-true')
+                grdBuy1.classList.add('button-false')  
+            }
+           
+        })
+
         grdBuy5.addEventListener('click', ()=>{
             grandma.buy5();
             update();
         });         
+
+       
 
         //farm's buy 1 and buy 5
         farmBuy1.addEventListener('click', ()=>{
