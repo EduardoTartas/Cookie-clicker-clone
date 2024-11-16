260ee8646671c1a00c4ba1044ecb3e0a0edf041e
Eduardo Tartas <eduardos.tartas@gmail.com>
2024-11-15 20:59:38 -0400

    separando a classe do index

---
+++ src/Entity.ts
@@ -0,0 +1,68 @@
+export class Entity {
+    private price:number;
+    private price5:number
+    private priceScale:number;
+    private quantity:number;
+    private cookiesValue:number;
+    private cookiesPerSecond:number;
+
+    constructor(price:number, cookiesValue:number, priceScale:number){
+        this.price = price;
+        this.price5 = price;
+        this.priceScale = priceScale;
+        this.quantity = 0;
+        this.cookiesValue = cookiesValue;
+        this.cookiesPerSecond = 0;
+    }
+
+    
+
+
+
+    cost5():number{     
+        this.price5 = this.price;
+        for (let i = 0; i < 5; i++) {
+            this.price5 = Math.round(this.price5 * this.priceScale);
+        }
+        return this.price5;
+    }
+
+    buy1() : void{
+        if(cookie.quantity>=this.price){
+            cookie.quantity -=this.price
+            this.price = Math.round(this.price * this.priceScale);
+            this.quantity ++;
+            cookie.cookiesPerSecond += this.cookiesValue;
+        }
+    }
+    
+    buy5() : void{
+        if(cookie.quantity>=this.cost5()){
+            cookie.quantity -= this.price5;
+            this.price = this.price5;
+            this.quantity += 5;
+            cookie.cookiesPerSecond += (this.cookiesValue * 5);
+            this.cost5();
+        }
+    }
+
+    public getPrice(): number {return this.price;}
+
+    public getQuantity(): number {return this.quantity;}
+
+    public getCookiesValue(): number {return this.cookiesValue;}
+
+    public getCookiesPerSecond(): number {return this.cookiesPerSecond;}
+
+
+    public setPrice(price: number): void {this.price = price;}
+
+    public setQuantity(quantity: number): void {this.quantity = quantity;}
+
+    public setCookiesValue(cookiesValue: number): void {this.cookiesValue = cookiesValue;}
+
+    public setCookiesPerSecond(cookiesPerSecond: number): void {this.cookiesPerSecond = cookiesPerSecond;}
+
+}
+
+ export const cookie = new Entity(0,0,0)
--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,10 @@
-class Entity {
-    private price:number;
-    private price5:number
-    private priceScale:number;
-    private quantity:number;
-    private cookiesValue:number;
-    private cookiesPerSecond:number;
-
-    constructor(price:number, cookiesValue:number, priceScale:number){
-        this.price = price;
-        this.price5 = price;
-        this.priceScale = priceScale;
-        this.quantity = 0;
-        this.cookiesValue = cookiesValue;
-        this.cookiesPerSecond = 0;
-    }
-
-    
-
-
-
-    cost5():number{     
-        this.price5 = this.price;
-        for (let i = 0; i < 5; i++) {
-            this.price5 = Math.round(this.price5 * this.priceScale);
-        }
-        return this.price5;
-    }
-
-    buy1() : void{
-        if(cookie.quantity>=this.price){
-            cookie.quantity -=this.price
-            this.price = Math.round(this.price * this.priceScale);
-            this.quantity ++;
-            cookie.cookiesPerSecond += this.cookiesValue;
-        }
-    }
-    
-    buy5() : void{
-        if(cookie.quantity>=this.cost5()){
-            cookie.quantity -= this.price5;
-            this.price = this.price5;
-            this.quantity += 5;
-            cookie.cookiesPerSecond += (this.cookiesValue * 5);
-            this.cost5();
-        }
-    }
-
-    public getPrice(): number {return this.price;}
-
-    public getQuantity(): number {return this.quantity;}
-
-    public getCookiesValue(): number {return this.cookiesValue;}
-
-    public getCookiesPerSecond(): number {return this.cookiesPerSecond;}
-
-
-    public setPrice(price: number): void {this.price = price;}
-
-    public setQuantity(quantity: number): void {this.quantity = quantity;}
-
-    public setCookiesValue(cookiesValue: number): void {this.cookiesValue = cookiesValue;}
-
-    public setCookiesPerSecond(cookiesPerSecond: number): void {this.cookiesPerSecond = cookiesPerSecond;}
-
-}
-
-const cookie = new Entity(0,0,0)
+import { Entity, cookie } from './Entity';
 
 const grandma = new Entity(50, 1, 1.15);
 
 const farm = new Entity(200, 2, 1.20);
 
 const mine = new Entity(500, 4, 1.40);
-
-let totalCookiePerSecond :number = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -98,9 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const minePrice5    = document.querySelector('#mine-price5')!;
     const mineBuy1     = document.querySelector('#buy-1-mine')!;
     const mineBuy5     = document.querySelector('#buy-5-mine')!;
-
-    const hover = document.querySelector('.button:hover')!;
-
 
     //status' update
     function update(): void{
@@ -134,7 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
             update()
         });
 
-         grdBuy1.addEventListener('mouseover', () => {
+        grdBuy5.addEventListener('click', ()=> {
+            grandma.buy5();
+            update();
+        });
+
+        //grandma's hover effect
+        grdBuy1.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= grandma.getPrice()){
                 grdBuy1.classList.add('button-active')
                 grdBuy1.classList.remove('button-false-hover')
@@ -146,11 +80,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 grdBuy1.classList.add('button-false-hover')  
             }
         })
-
-        grdBuy5.addEventListener('click', ()=> {
-            grandma.buy5();
-            update();
-        });
 
         grdBuy5.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= grandma.cost5()){
@@ -165,17 +94,19 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         
-        
-           
-
-       
-
         //farm's buy 1 and buy 5
         farmBuy1.addEventListener('click', ()=>{
             farm.buy1();
             update()
         });
 
+       
+        farmBuy5.addEventListener('click', ()=>{
+            farm.buy5();
+            update();
+        });     
+
+        //farm's hover effect
         farmBuy1.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= farm.getPrice()){
                 farmBuy1.classList.add('button-active')
@@ -189,10 +120,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
 
-        farmBuy5.addEventListener('click', ()=>{
-            farm.buy5();
-            update();
-        });     
         
         farmBuy5.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= farm.cost5()){
@@ -207,12 +134,18 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         
-        //farm's buy 1 and buy 5
+        //mine's buy 1 and buy 5
         mineBuy1.addEventListener('click', ()=>{
             mine.buy1();
             update()
         });
 
+        mineBuy5.addEventListener('click', ()=>{
+            mine.buy5();
+            update();
+        });    
+
+        //farm's hover effect
         mineBuy1.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= farm.getPrice()){
                 mineBuy1.classList.add('button-active')
@@ -226,11 +159,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
 
-        mineBuy5.addEventListener('click', ()=>{
-            mine.buy5();
-            update();
-        });    
-        
         mineBuy5.addEventListener('mouseover', () => {
             if(cookie.getQuantity()>= mine.cost5()){
                 mineBuy5.classList.add('button-active')
@@ -244,6 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
 
+    //add cookies per second to the total
     setInterval(() => {
         cookie.setQuantity(cookie.getQuantity() + cookie.getCookiesPerSecond());
         update();
